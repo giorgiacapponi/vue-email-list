c448bd60025de8cca9ef57f6c882ca3808c00a44
giorgiacapponi <giorgiacapponi3@gmail.com>
2023-04-11 19:15:34 +0200

    b

--- js/script.js
+++ js/script.js
@@ -2,26 +2,24 @@ const {createApp}=Vue;
 createApp({
     data(){
         return{
-            newEmail:this.rndEmail()
-              
+            newEmail:[],
+           
+
         }
     },
+
+    
     methods:{
         rndEmail(){
-            let email=[]
             for(let i=0;i<30;i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                  .then((resp)=>{
                   
-                    email.push(resp.data.response)
+                    this.newEmail.push(resp.data.response)
                    
                  })
             }
-            console.log(email);
-           return email
-             
-            
         }
 
-    },
+    }
 }).mount('#app')
